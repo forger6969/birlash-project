759fc2e200998280c0880ba1819abc3b2ad08532
forger6969 <saidazim186@gmail.com>
2025-11-26 16:23:32 +0500

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Home from './Pages/Home'
+import Loading from './Pages/Loading'
 
 const App = () => {
+  const [loader, setLoader] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoader(false)
+    }, 2000);
+  }, [])
   return (
     <div>
 
-      <Home />
+      {loader ? <Loading /> :
+        <Home />
+      }
+
     </div>
   )
 }
