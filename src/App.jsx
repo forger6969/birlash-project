db0f0356fe7118787328f9744d669c0aec51e1ed
amangel1ev.10 <hp8187081014laptop@gmail.com>
2025-11-26 16:24:21 +0500

    Aziz

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Home from './Pages/Home'
+import { Foters } from './Components/Foters'
 
 const App = () => {
   return (
     <div>
 
       <Home />
+      <Foters/>
     </div>
   )
 }
